aa0cdd21054c5f5bfe2087413be31c23523844d3
Idrisov Sergey <idrisovsergei@phystech.edu>
2024-03-26 09:00:35 +0300

    f

--- 2_semestr/seminar_6/zadacha_from_HW.py
+++ 2_semestr/seminar_6/zadacha_from_HW.py
@@ -9,24 +9,29 @@ def init():
         return  condition, znak
 def check(condition):
     znak = condition[1]
-    for i in range(len(znak)):
-        if znak[i] :
-            return True
     condition = condition[0]
-
+    for i in range(len(znak)-1):
+        if znak[i] ==znak[i+1] or (znak[i] == '<=' and znak[i+1] == '>='):
+            return True
+        else:
+            pass
 def sort(condition):
+        znak = condition[-1]
         condition = condition[0]
+        znak_sort = ['']* len(znak)
         q = len(condition)
         condition_sort = [[1, 100000000000, 0] for _ in range(q)]
         for i in range(q):
             for j in range(q):
                 if condition_sort[i][1] - condition_sort[i][0] > condition[j][1] - condition[j][0] and condition_sort.count(condition[j])==False:
                     condition_sort[i] = condition[j]
+                    znak_sort[i] = znak[j]
+        condition_sort.append(znak_sort)
         return condition_sort
 
 condition = init()
-sort(condition)
-check(condition)
+print(sort(condition))
+print(check(condition))
 """
 3
 2 4 3
